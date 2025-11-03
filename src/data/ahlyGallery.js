e80653d709f68964230ac741a1781ecc099fbb60
alinasr783 <alinasreldin783@gmail.com>
2025-11-03 13:16:11 +0200

    edit 11

--- src/data/ahlyGallery.js
+++ src/data/ahlyGallery.js
@@ -1,37 +1,3 @@
 export const ahlyGallery = [
-  {
-    src: 'https://scontent.fcai21-3.fna.fbcdn.net/v/t39.30808-6/482325777_964773065809745_1232908204418131703_n.jpg?stp=dst-jpg_s590x590_tt6&_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeGpLa3k4NlFNf71kbPrt3PNFH3NFTlLdT8Ufc0VOUt1P35QNGIsy_VojwPHn4BllpPYN9DyInrz3a64pn3bJspq&_nc_ohc=IqvQkcCAWP4Q7kNvwHK3v_x&_nc_oc=Adl7CAQ7mHEDkk247orhgYn5_HfVJ794z9DpApHXma6INNzxZUcQ0ZPFpnDVuprbZIE&_nc_zt=23&_nc_ht=scontent.fcai21-3.fna&_nc_gid=SxtijQNa1ap2Defmc7Dpaw&oh=00_AfelqX7otTMuWp3angLQBDGAQ7s9nlKPAi_rLxNrK_3BsQ&oe=69089D51',
-    caption: 'On-field emergency transfer management',
-    tags: ['Matchday', 'Emergency', 'Al Ahly'],
-  },
-  {
-    src: 'https://scontent.fcai21-3.fna.fbcdn.net/v/t39.30808-6/482826715_964773049143080_4995424134019958915_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFLylKPxbFzVNwZZqvPPRzeGOso_nHvCTUY6yj-ce8JNScoVFerDAFjBbgmzJ50OBNOCT88xSdPnp3qCzYcdwKS&_nc_ohc=sYiTYdZ7Ee8Q7kNvwFtJvOz&_nc_oc=AdnI-M9N8nAG5Pnxhr6fdqYZDsdeKqP9LputNTf_BORd0B-rTQdkn6x3FnYAVu9fyz8&_nc_zt=23&_nc_ht=scontent.fcai21-3.fna&_nc_gid=YBn3hKHvVK2ZY1E8ZUHzGQ&oh=00_AffpzFyqU50S_LqQ_JMxgwPze-0mggF9m9Qu6-97fdHcTw&oe=6908B9F9',
-    caption: 'Player recovery and rehabilitation support',
-    tags: ['Rehab', 'Medical Room', 'Al Ahly'],
-  },
-  {
-    src: 'https://scontent.fcai21-3.fna.fbcdn.net/v/t39.30808-6/482081164_964174025869649_868483409263252211_n.jpg?stp=dst-jpg_s720x720_tt6&_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeGCkMlhRaEPAewkiFU8kufrdI15CqgYVRV0jXkKqBhVFYxXSWV2Z5EsZAGMN9EEle7NgW9-wfnXtn-YS8ZxJNH1&_nc_ohc=JGyIBWwBalUQ7kNvwEnly7Y&_nc_oc=AdlkI0VThiuH5qRvH40q6cdQgfv45_Kc2QhoTHLAA97wk9m-TzV7-SaBpnc8zENk7so&_nc_zt=23&_nc_ht=scontent.fcai21-3.fna&_nc_gid=YBn3hKHvVK2ZY1E8ZUHzGQ&oh=00_AfeGTi2ZAahJfkFIjFoIqGNc6E6G69eKg8wg-ZCsq4qBzg&oe=6908A629',
-    caption: 'U17 top scorer returns after ACL recovery',
-    tags: ['ACL', 'Team', 'Al Ahly'],
-  },
-  {
-    src: 'https://scontent.fcai21-3.fna.fbcdn.net/v/t39.30808-6/481804999_962167982736920_1575712398379750551_n.jpg?stp=dst-jpg_p180x540_tt6&_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFl-3Y5BFmBtXJuxIPCqkmZ5i0-I5bZonvmLT4jltmiewdLnEf1PYlyP1NlnvwvKt1Y-8OQBlgbl_z2hwvA1j2Y&_nc_ohc=nyWSoHdrRKkQ7kNvwEWxEsy&_nc_oc=AdkuVd_1q4ZWvLaouCkyqESGYONecFB7dGECHOZ0Yseazy_6hX8S2S4UUI1grguHD0I&_nc_zt=23&_nc_ht=scontent.fcai21-3.fna&_nc_gid=vDp84WsLhBwJKNLETi6yjA&oh=00_AfdPvIWQwKJhPDwgtdYULPdZF2UY5JLzzBK4ZvrkAD3e1w&oe=6908B820',
-    caption: 'Advancing sports medicine practices',
-    tags: ['Sports Medicine', 'Team', 'Al Ahly'],
-  },
-  , {
-    src: 'https://scontent.fcai21-3.fna.fbcdn.net/v/t39.30808-6/481988553_960997559520629_5070114223976650106_n.jpg?stp=dst-jpg_s206x206_tt6&_nc_cat=107&ccb=1-7&_nc_sid=714c7a&_nc_eui2=AeGsJw9EPRJ3Rbx2WuqNmuDcFs3oujecSEUWzei6N5xIRV2UEhdjhH7_pI7f0BoYuI-DpvuIIz8qX50-NoQgpDw0&_nc_ohc=K4AKt8oXo9kQ7kNvwEMiJ_3&_nc_oc=AdkgjMdhShl9dkFc6K_ZWJL6dJPj5cPFKxhZvwuf2o1i9_namLd9xZmU_v6Jjvx6_do&_nc_zt=23&_nc_ht=scontent.fcai21-3.fna&_nc_gid=Znljh5kuFv34l76XyeB-Gw&oh=00_AfiQ3-jr5GPAgqybR3usiKG3-5YdgtisNQBm4hgImUPaEQ&oe=690CF9D6',
-    caption: 'Pitch-side medical care and team support',
-    tags: ['Matchday', 'Al Ahly'],
-  },
-  {
-    src: 'https://scontent.fcai21-2.fna.fbcdn.net/v/t39.30808-6/481659091_960997862853932_5150232325296068012_n.jpg?stp=dst-jpg_s206x206_tt6&_nc_cat=111&ccb=1-7&_nc_sid=714c7a&_nc_eui2=AeFwVgmhdNBEfsPk192BeYkLl9G_ORcLADGX0b85FwsAMUXjScL6hkGxNjeDatuQlW_-BmZh-dA3V7PsnrKXkY23&_nc_ohc=BLot5VkbIF8Q7kNvwFD_oC3&_nc_oc=AdnubMhUaUs8mpJIhnQ_HG7B69YAWj1yGGoZ3UXIA2K0h1ZbU3Jl6fB4C5VL0tUUKbE&_nc_zt=23&_nc_ht=scontent.fcai21-2.fna&_nc_gid=Znljh5kuFv34l76XyeB-Gw&oh=00_AfisUSsmn8DvGykqHR6U468R7AZtYjbzaB3uQVK6hXmgkw&oe=690CD6E3',
-    caption: 'Sideline care and injury prevention',
-    tags: ['Matchday', 'Al Ahly'],
-  },
-  {
-    src: 'https://scontent.fcai21-4.fna.fbcdn.net/v/t39.30808-6/482322002_964772965809755_8820540162602210369_n.jpg?stp=dst-jpg_s206x206_tt6&_nc_cat=104&ccb=1-7&_nc_sid=50ad20&_nc_eui2=AeEKDDNh3o7hlqDFg5pV-093N1Nb7Himmfk3U1vseKaZ-f8uzsJdrBrGzSJ_lu3t3qTE-HMtw5VULgaQFt44DZ8m&_nc_ohc=CZBOppokiUsQ7kNvwEE-Xe9&_nc_oc=Adlmw27qC5GfUlreC2uUb8k-MxQS76Klw352OoLR_X8wlObXRoDepnnRtiEv50invI8&_nc_zt=23&_nc_ht=scontent.fcai21-4.fna&_nc_gid=hpX0IwQih9N1NgXfTe69iQ&oh=00_AfjKiZXBZWWw3wJLBgA3nILB3aWOsXHLYtpJz45Em4BU3w&oe=690CD4BC',
-    caption: 'Emergency response coordination at match venue',
-    tags: ['Matchday', 'Emergency', 'Al Ahly'],
-  }
+ 
 ]
